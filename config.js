526052b8970ca946a507651367e8ffc3bb5d7fa7
ReichMax <38127253+reichmachine@users.noreply.github.com>
2026-09-22 01:17:44 +0200

    Usar comidas.reichmax.com para acceso e invitaciones

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 window.SAIH_CONFIG = {
   "supabaseUrl": "https://keajharsdoyndipfijuo.supabase.co",
-  "siteUrl": "https://reichmachine.github.io/comidas-saih/",
+  "siteUrl": "https://comidas.reichmax.com/",
   "publishableKey": "sb_publishable_Cc6IS8D15QXyrgLePKPhfw_Zr06Ht8_",
   "mode": "online"
 };
+
