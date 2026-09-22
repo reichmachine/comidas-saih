e556aaaced1f1ac52ab641166190ae6244bd4fa9
ReichMax <38127253+reichmachine@users.noreply.github.com>
2026-09-22 13:06:55 +0200

    Activar invitaciones por correo con reintentos seguros

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ window.SAIH_CONFIG = {
   "supabaseUrl": "https://keajharsdoyndipfijuo.supabase.co",
   "siteUrl": "https://comidas.reichmax.com/",
   "publishableKey": "sb_publishable_Cc6IS8D15QXyrgLePKPhfw_Zr06Ht8_",
-  "mode": "online"
+  "mode": "online", "emailEnabled": true
 };
+
 
